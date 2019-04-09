638cebe7c089a3cb68a2ab7b99c2b6f243221d18
RefanoAndreas <refanoandreas@gmail.com>
2019-04-09 17:01:04 +0700

    09/04/2019

--- app/src/main/java/com/example/travelgo/MainActivity.java
+++ app/src/main/java/com/example/travelgo/MainActivity.java
@@ -2,39 +2,14 @@ package com.example.travelgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    RecyclerView listMenu;
-    ArrayList<String> menu;
-    MainList adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listMenu =  (RecyclerView) findViewById(R.id.list);
-
-        adapter = new MainList(menu, this);
-
-        RecyclerView.LayoutManager mLayoutmanager  = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
-        listMenu.setLayoutManager(mLayoutmanager);
-        listMenu.setItemAnimator(new DefaultItemAnimator());
-        listMenu.setAdapter(adapter)
-        listMenu.setNestedScrollingEnabled(false);
-
-//        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listMenu);
-//        listMenu.setAdapter(listAdapter);
     }
 
 
---
+++ app/src/main/java/com/example/travelgo/ProfileEdit.java
@@ -0,0 +1,27 @@
+package com.example.travelgo;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_edit);
+
+        ((TextInputLayout) findViewById(R.id.TIL_name_profileEdit)).setHint(null);
+        ((TextInputEditText) findViewById(R.id.TIET_name_profileEdit)).setHint(null);
+        ((TextInputLayout) findViewById(R.id.TIL_email_profileEdit)).setHint(null);
+        ((TextInputEditText) findViewById(R.id.TIET_email_profileEdit)).setHint(null);
+        ((TextInputLayout) findViewById(R.id.TIL_phone_profileEdit)).setHint(null);
+        ((TextInputEditText) findViewById(R.id.TIET_phone_profileEdit)).setHint(null);
+        ((TextInputLayout) findViewById(R.id.TIL_password_profileEdit)).setHint(null);
+        ((TextInputEditText) findViewById(R.id.TIET_password_profileEdit)).setHint(null);
+        ((TextInputLayout) findViewById(R.id.TIL_rePassword_profileEdit)).setHint(null);
+        ((TextInputEditText) findViewById(R.id.TIET_rePassword_profileEdit)).setHint(null);
+    }
+
+}
