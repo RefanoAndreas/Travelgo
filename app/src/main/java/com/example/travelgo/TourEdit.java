6ce72bad246a99b7190903bbc2391f6d1b244973
RefanoAndreas <refanoandreas@gmail.com>
2019-04-10 16:56:17 +0700

    10/04/2019
    - Front End Dev's Notes :
      - Edit Tour Created (base, 3 Screen Size, 2 Orientation)
      - List Tour Created (base, without RecyclerView)

---
+++ app/src/main/java/com/example/travelgo/TourEdit.java
@@ -0,0 +1,20 @@
+package com.example.travelgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class TourEdit extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_edit);
+//
+//        imageView = (ImageView) findViewById(R.id.image_tourEdit);
+//        imageView.setClipToOutline(true);
+
+    }
+}
---
+++ app/src/main/java/com/example/travelgo/TourListPackages.java
@@ -0,0 +1,13 @@
+package com.example.travelgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TourListPackages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_list_packages);
+    }
+}
