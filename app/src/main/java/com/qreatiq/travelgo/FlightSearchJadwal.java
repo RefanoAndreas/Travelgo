cecab61d124b663473bdf840e2073ebc40b1892b
RefanoAndreas <refanoandreas@gmail.com>
2019-04-29 16:58:16 +0700

    Flight Search Jadwal

--- app/src/main/java/com/qreatiq/travelgo/FlightSearchJadwal.java
+++ app/src/main/java/com/qreatiq/travelgo/FlightSearchJadwal.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -23,6 +24,10 @@ public class FlightSearchJadwal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_search_jadwal);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mRecyclerView = findViewById(R.id.RV_chooseDate);
         mRecyclerView.setHasFixedSize(true);
