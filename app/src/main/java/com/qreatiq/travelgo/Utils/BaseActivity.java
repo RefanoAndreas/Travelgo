92cbebdbe66cab6273ff75670e2bf9aa9ed4b279
joshua <jbuwono@gmail.com>
2019-08-02 15:20:19 +0700

    edit by josh

--- app/src/main/java/com/qreatiq/travelgo/Utils/BaseActivity.java
+++ app/src/main/java/com/qreatiq/travelgo/Utils/BaseActivity.java
@@ -88,7 +88,7 @@ public class BaseActivity extends AppCompatActivity {
 //    public String C_URL_IMAGES = "http://travelgolaravel.propertigo.id/api/images/";
 
     public boolean production = true;
-    public String version = "2.9";
+    public String version = "3.0";
 
     public Uri camera_uri;
 
